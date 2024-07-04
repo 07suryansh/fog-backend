0e259c0048a9fce59af31b0c9f5f11df22ba2983
Suryansh Singh Bisen <07suryansh@gmail.com>
2024-07-04 21:47:39 +0530

    updated coors

--- index.js
+++ index.js
@@ -8,20 +8,29 @@ import advancedRoutes from './routes/advancedRoutes.js';
 import ruleRoutes from './routes/ruleRoutes.js';
 import pingRoutes from './routes/pingRoutes.js';
 import tickrateRoutes from './routes/tickrateRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 
+const corsOptions = {
+    origin: 'https://fog-frontend.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 connectDB();
 
-app.use('/api/settings', settingRoutes);
-app.use('/api/players', playerRoutes);
-app.use('/api/advanced', advancedRoutes);
-app.use('/api/rules', ruleRoutes);
-app.use('/api/ping', pingRoutes);
-app.use('/api/tickrate', tickrateRoutes);
+app.use('/settings', settingRoutes);
+app.use('/players', playerRoutes);
+app.use('/advanced', advancedRoutes);
+app.use('/rules', ruleRoutes);
+app.use('/ping', pingRoutes);
+app.use('/tickrate', tickrateRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
